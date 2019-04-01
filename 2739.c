f9b8773c8817eac03b3d6fc7ca6d33565c7e76b8
wpgur <48816887+wpgur@users.noreply.github.com>
2019-04-01 14:14:43 +0900

    Add files via upload

---
+++ 2739.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int N;
+
+	scanf("%d", &N);
+
+	printf("%d * 1 = %d\n", N, (N * 1));
+	printf("%d * 2 = %d\n", N, (N * 2));
+	printf("%d * 3 = %d\n", N, (N * 3));
+	printf("%d * 4 = %d\n", N, (N * 4));
+	printf("%d * 5 = %d\n", N, (N * 5));
+	printf("%d * 6 = %d\n", N, (N * 6));
+	printf("%d * 7 = %d\n", N, (N * 7));
+	printf("%d * 8 = %d\n", N, (N * 8));
+	printf("%d * 9 = %d\n", N, (N * 9));
+
+	return 0;
+
+}
