9bc0f46529ac0fdeb8cd128114e6d80f14a99d4b
wpgur <48816887+wpgur@users.noreply.github.com>
2019-04-10 22:39:58 +0900

    Add files via upload

---
+++ 2438.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int s,i,j;
+
+
+	scanf_s("%d", &s);
+	for (i = 1; i <= s; i++) {
+		for (j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	
+
+
+	return 0;
+}
