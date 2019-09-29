2d513e1baff72bc9670b07ff2296e108219bfc75
wpgur <48816887+wpgur@users.noreply.github.com>
2019-09-30 03:22:16 +0900

    Add files via upload

---
+++ 10869.py
@@ -0,0 +1,19 @@
+A,B=input().split()
+A=int(A)
+B=int(B)
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+
+'''A=int(input('A입력:'))
+B=int(input('B입력:'))
+print('%d\n%d\n%d\n%d\n%d' % (A+B,A-B,A*B,A//B,A%B))
+'''
+'''
+A,B=input('A와B입력:').split()
+A=int(A)
+B=int(B)
+'''
